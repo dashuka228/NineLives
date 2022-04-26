4e2aa20a1b87324e28c55a68229a2a7d20587acd
dashuka228 <lukyanovadasha17@gmail.com>
2022-04-27 00:40:30 +0300

    урон через 5 секунд

--- NineLives/Assets/Scripts/PlayerInteraction.cs
+++ NineLives/Assets/Scripts/PlayerInteraction.cs
@@ -8,20 +8,60 @@ public class PlayerInteraction : MonoBehaviour
     public int health = 9;
     public static PlayerInteraction Instance { get; set; }
     public string scene;
+    
 
     void Start()
     {
         scene = SceneManager.GetActiveScene().name;
         Instance = this;
     }
+
+    private void Update()
+    {
+        
+    }
+
+    //private bool invincible = false;
+    //public float invincibilityTime = 3f;
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (!invincible)
+    //    {
+    //        if (collision.gameObject.tag == "enemy")
+    //        {
+    //            StartCoroutine(Invulnerability());
+    //        }
+    //    }
+    //}
+
+    //IEnumerator Invulnerability()
+    //{
+    //    invincible = true;
+    //    yield return new WaitForSeconds(invincibilityTime);
+    //    invincible = false;
+    //}
+
+
+    private bool invincible = false;
+
     public void TakeDamage(int damage)
     {
-        health -= damage;
+        if (!invincible)
+        {
+            health -= damage;
+            invincible = true;
+            Invoke("resetInvulnerability", 5);
+        }
 
         if (health <= 0)
         {
             Die();
         }
+    }
+    void resetInvulnerability()
+    {
+        invincible = false;
     }
 
     //переделать потом на другое 
